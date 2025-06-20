e70abb9bbe54214abb8400e0633d3f5268ffeadc
Greeshma-jagarapu <jagarapugreeshma2003@gmail.com>
2025-06-20 13:20:47 +0530

    added both frontend and backend folders

---
+++ frontend/src/components/AuthImagePattern.jsx
@@ -0,0 +1,15 @@
+import  welcomeImg  from "../assets/welcomeImg.png";
+
+const AuthImagePattern = ({ title, subtitle }) => {
+    return (
+      <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+        <div className="max-w-md text-center">      
+          <img src={welcomeImg} />
+          <h2 className="text-2xl font-bold mb-4">{title}</h2>
+          <p className="text-base-content/60">{subtitle}</p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default AuthImagePattern;
